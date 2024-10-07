d2a52ba36be9ce5919d0a781ac0c9062c88539ae
MohsEin Maner <mohsin.pgmr@gmail.com>
2024-10-07 13:15:29 +0530

    Add project files.

---
+++ js/sitescripts.js
@@ -0,0 +1,15 @@
+﻿function ManageSticky() {
+    var header = document.getElementById("header");
+    var logoMain = document.getElementById("logo");
+    var pageHeight = window.innerHeight;
+    var sticky = header.offsetTop;
+    if (window.pageYOffset > sticky) {
+        header.classList.add("is-sticky");
+        logoMain.style.width = "60px";
+        
+    } else {
+        header.classList.remove("is-sticky");
+        logoMain.style.width = "100px";
+        
+    }
+}
